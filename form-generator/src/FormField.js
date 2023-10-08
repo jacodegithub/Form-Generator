7593215175fef6676e137eba4fdb854882451b2d
jacodegithub <dudeobama246@gmail.com>
2023-10-08 12:59:48 +0530

    adding validation

--- form-generator/src/FormField.js
+++ form-generator/src/FormField.js
@@ -10,24 +10,50 @@ require("formBuilder/dist/form-render.min.js")
 
 const FormRender = () => {
     const { form, setForm } = useContext(FormContext);
+    const [errors, setErrors] = useState({})
     const [ display, setDisplay] = useState({});
     const fb = createRef();
-    let formRender;
 
 
     useEffect(() => {
         let formData = form;
-        formRender = $(fb.current).formRender( {formData} );
+        $(fb.current).formRender( {formData} );
     }, [form]);
 
     function saveData() {
-        setDisplay($(fb.current).formRender("userData"))
-    }
+        const render = $(fb.current).formRender();
+        const emptyFields = [];
+        render.find("[required]").each(function () {
+          if ($(this).val().trim() === "") {
+            emptyFields.push($(this).attr("name"));
+          }
+        });
+    
+        if (emptyFields.length > 0) {
+          const newErrors = {};
+          emptyFields.forEach((fieldName) => {
+            newErrors[fieldName] = "This field is required.";
+          });
+          setErrors(newErrors);
+        } else {
+          setErrors({});
+          setDisplay(render.formRender("userData"));
+        }
+      }
+    
     return (
         <div className="display-form">
             <h1>Display Form</h1>
             <form ref={fb}></form>
             
+            {Object.keys(errors).length > 0 && (
+                <div className="error-messages">
+                    {Object.keys(errors).map((fieldName, index) => (
+                        <p key={index}>{errors[fieldName]}</p>
+                    ))}
+                </div>
+            )}
+
             <>
               <button
               onClick={saveData}
