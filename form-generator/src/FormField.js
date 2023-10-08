e851f4073f8d687e1b756e5cfe49412883ac9b65
jacodegithub <dudeobama246@gmail.com>
2023-10-08 13:13:34 +0530

    removing issues

--- form-generator/src/FormField.js
+++ form-generator/src/FormField.js
@@ -10,7 +10,6 @@ require("formBuilder/dist/form-render.min.js")
 
 const FormRender = () => {
     const { form, setForm } = useContext(FormContext);
-    const [errors, setErrors] = useState({})
     const [ display, setDisplay] = useState({});
     const fb = createRef();
 
@@ -21,39 +20,13 @@ const FormRender = () => {
     }, [form]);
 
     function saveData() {
-        const render = $(fb.current).formRender();
-        const emptyFields = [];
-        render.find("[required]").each(function () {
-          if ($(this).val().trim() === "") {
-            emptyFields.push($(this).attr("name"));
-          }
-        });
-    
-        if (emptyFields.length > 0) {
-          const newErrors = {};
-          emptyFields.forEach((fieldName) => {
-            newErrors[fieldName] = "This field is required.";
-          });
-          setErrors(newErrors);
-        } else {
-          setErrors({});
-          setDisplay(render.formRender("userData"));
-        }
-      }
-    
+        setDisplay($(fb.current).formRender("userData"))
+    }
     return (
         <div className="display-form">
             <h1>Display Form</h1>
             <form ref={fb}></form>
             
-            {Object.keys(errors).length > 0 && (
-                <div className="error-messages">
-                    {Object.keys(errors).map((fieldName, index) => (
-                        <p key={index}>{errors[fieldName]}</p>
-                    ))}
-                </div>
-            )}
-
             <>
               <button
               onClick={saveData}
@@ -62,7 +35,9 @@ const FormRender = () => {
                 Submit Data
               </button>           
             </>
-
+            {Object.keys(display).length > 0 &&
+                <p>{JSON.parse(JSON.stringify(display))}</p>
+              }
         </div>
     )
 }
